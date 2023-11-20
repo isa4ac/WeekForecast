2a22244ac73fbb164459a38c02c5f0ef502a7ea7
Isaac Van Meter <ivanmeter28@gmail.com>
2023-11-20 14:37:12 -0500

    Initial Commit

---
+++ WeekForecast/WeekForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeekForecastApp.swift
+//  WeekForecast
+//
+//  Created by Isaac Vanmeter on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WeekForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
